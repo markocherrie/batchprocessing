af0155e7109a6b874cf769dbb5fc8d4554d44bdb
markocherrie <m.cherrie@ed.ac.uk>
2017-09-27 20:23:12 +0100

    Changed download to catch error in downloading, __le instead of __8b

--- download.R
+++ download.R
@@ -1,6 +1,6 @@
 setwd("C:/Users/mcherrie/batchprocessing")
 
-datetoday<-gsub("-","", Sys.Date()-1)
+dateyesterday<-gsub("-","", Sys.Date()-1)
 #download file
 UVdownload <-function(sat, date, UVRtype, temporal){
   tryCatch(
@@ -14,10 +14,22 @@ UVdownload <-function(sat, date, UVRtype, temporal){
       file<-paste0("MOD", "02SSH_A", date , "Av1_v601_7200_3601_",UVRtype,"__8b.gz")
       searchFTP<-paste0(FTP, "MOD", "02SSH_A", date, "Av1_v811_7200_3601_",UVRtype,"__8b.gz")
       download.file(searchFTP, destfile=paste0(getwd(), "/rawdata/", sub(FTP, "", searchFTP)))
+    } , error=function(e){
+      cat("ERROR :",conditionMessage(e), "\n")
+      FTP <- paste0("ftp://apollo.eorc.jaxa.jp/pub/JASMES/Global_05km/", UVRtype, "/", temporal,"/", substring(date,1,6), "/")
+      file<-paste0("MOD", "02SSH_A", date , "Av1_v601_7200_3601_",UVRtype,"__le.gz")
+      searchFTP<-paste0(FTP, "MOD", "02SSH_A", date, "Av1_v811_7200_3601_",UVRtype,"__le.gz")
+      download.file(searchFTP, destfile=paste0(getwd(), "/rawdata/", sub(FTP, "", searchFTP)))
+    } , error=function(e){
+      cat("ERROR :",conditionMessage(e), "\n")
+      FTP <- paste0("ftp://apollo.eorc.jaxa.jp/pub/JASMES/Global_05km/", UVRtype, "/", temporal,"/", substring(date,1,6), "/")
+      file<-paste0("MYD", "02SSH_A", date , "Av1_v601_7200_3601_",UVRtype,"__le.gz")
+      searchFTP<-paste0(FTP, "MOD", "02SSH_A", date, "Av1_v811_7200_3601_",UVRtype,"__le.gz")
+      download.file(searchFTP, destfile=paste0(getwd(), "/rawdata/", sub(FTP, "", searchFTP)))
     })
 }
 ###########################################################
-UVdownload("MYD", datetoday, "uvb", "daily")
+UVdownload("MYD", dateyesterday, "uvb", "daily")
 ###########################################################
 
 
